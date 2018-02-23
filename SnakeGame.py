455fa85627cdf35a9f33ae479302f8c0f478bdd2
Caleb Brody <Caleb.Brody@outlook.com>
2018-02-22 18:12:51 -0800

    Update

---
+++ SnakeGame.py
@@ -0,0 +1,166 @@
+#Code to play the game snake and write a gif
+#Writen by Caleb Brody
+#Inspired by University of Texas at Austin paper
+#http://nn.cs.utexas.edu/downloads/papers/stanley.gecco02_1.pdf
+
+import gc
+import imageio
+from PIL import Image
+import numpy as np
+import pandas as pd
+MapSize=16
+Px_Size=int(600/MapSize)
+
+class Location:
+	def __init__(self, x=None, y=None):
+		if x is None:
+			self.loc = np.array([ np.random.randint(MapSize),np.random.randint(MapSize)] )
+		else:
+			self.loc = np.array((x,y))
+			
+	def __eq__(self, other):
+		return np.product(self.loc == other.loc)
+		
+	def __add__(self, other):
+		return self.loc + other.loc
+		
+	def __str__(self):
+		return self.loc.__str__()
+		
+	def move(self, other):
+		self.loc +=other.loc
+		
+	def turn(self, by):
+		if by==0:
+			pass
+		elif by==1:
+			self.loc=np.array((self.loc[1], self.loc[0]))
+		elif by==2:
+			self.loc=-np.array((self.loc[1], self.loc[0]))
+		else:
+			raise ValueError("cant turn by" + str(by))
+			
+	def copy(self):
+		return Location(self.loc[0], self.loc[1])
+		
+	def CardinalDirections(self):
+		dict={}
+		for i in range(3):
+			tmp=self.copy()
+			tmp.turn(i)
+			dict[i]=tmp
+		return dict
+		
+	def oob(self): #Out of bounds
+		return np.sum(self.loc<0)>0 or np.sum(self.loc>MapSize )>0 
+	
+		
+game = pd.DataFrame()
+
+def PlayGame(brain, save=False, TurnMax=400, Hunger=99999):
+	StartGame()
+	Alive = True
+	turn = 0
+	VisibleState =[0]*9
+	gif, StateLog, RewardLog  = [], [],[]
+	HungerCounter=0
+	while Alive and HungerCounter<Hunger and turn < TurnMax:
+		turn += 1
+		decision = brain( VisibleState)
+		StateLog.append(VisibleState + VectoriseDecision(decision))
+		VisibleState , reward = MoveSnake(decision)
+		RewardLog.append( reward )
+		if reward < 0:
+			Alive = False
+		elif reward > 0 :
+			HungerCounter=0
+		else:
+			HungerCounter+=1
+		if save:
+			if turn % 100==0:
+				gc.collect()
+
+			gif.append(DrawBoard())
+	StateLog.append([0]*12)
+	return gif, StateLog, RewardLog
+	
+def AddFood():
+    game.food=Location()
+    if game.food in game.tail:
+        AddFood()
+
+def StartGame():
+	game.snake=Location()
+	game.tail=[game.snake.copy()]
+	game.head=Location(0,1)
+	AddFood()
+
+def RandomMove(VisibleState):
+	return np.random.randint(3)
+def MinimumDistance2Target(x,y, loc, target,default=0):
+    if (loc[0]-target[0])*(y-1) == (loc[1]-target[1]) *(x-1) and sum(loc!=target):
+        return max(-(loc[0]-target[0])*(x-1), -(loc[1]-target[1])*(y-1))
+    return default
+def VectoriseDecision(x):
+		return [x==0, x==1, x==2]
+		
+def MoveSnake(angle):
+	game.head.turn(angle)
+	game.snake.move(game.head)
+	
+	reward=0
+	if game.snake in game.tail:
+		reward=-.99
+	else:
+		game.tail.append(game.snake.copy())
+	if game.snake.oob():
+		reward=-1
+	if game.snake==game.food:
+		reward+=1
+		AddFood()
+	else:
+		game.tail=game.tail[1:]
+
+	return GetVisibleState() , reward
+
+def GetVisibleState():
+	CardinalDirections = game.head.CardinalDirections()
+	
+	WallRing, FoodRing, SelfRing = [], [], []
+	for i in range(3):
+		x,y = CardinalDirections[i].loc+1
+		wall=np.array([(x>0)*MapSize , (y>0)*MapSize ])
+		dist = np.abs( game.snake.loc-wall )
+		if y==1:
+			WallRing.append( dist[0]) 
+		elif x==1:
+			WallRing.append(dist[1]) 
+		FoodRing.append( MinimumDistance2Target(x,y, game.snake.loc, game.food.loc ) )
+		SelfRing.append( np.min( [MinimumDistance2Target(x,y, game.snake.loc, i.loc, MapSize*2 ) for i in game.tail]) )
+	return WallRing + FoodRing + SelfRing
+
+def ScaleFor3dy(x,z, th=.75):
+	return int  (x*(th**(z/MapSize)) * Px_Size )
+	
+def ScaleFor3dx(x, z, th=.85):
+	return int  (((x-MapSize/2.)*(th**(z/Px_Size/MapSize)) + MapSize/2.) * Px_Size )
+	
+def DrawBox(i, board, color, th=.75):
+	x, y = i.loc 
+	
+	for height in range( ScaleFor3dy(y,y) , ScaleFor3dy(y+1,y+1) ):
+		board[-height,  ScaleFor3dx(x, height+0., th=.75) :  ScaleFor3dx(x+1, height+0., th=.75) ]=color
+	
+def DrawBoard():
+	board = np.zeros((MapSize*Px_Size+Px_Size+1, MapSize*Px_Size+Px_Size+1, 3), dtype=np.uint8)
+	for i in game.tail:
+		if not i.oob(): 
+			DrawBox(i, board, [255,0,0])
+	DrawBox(game.food, board, [0,255,0])
+	
+	#board = Image.fromarray(board, 'RGB')
+	return board
+
+if __name__ == "__main__":
+	gif, _, __= PlayGame(RandomMove,  save=True)
+	imageio.mimsave("game.gif", gif)			
---
+++ SnakeLearn.py
@@ -0,0 +1,121 @@
+#Code to play the game snake and write a gif
+#Writen by Caleb Brody
+#Inspired by University of Texas at Austin paper
+#http://nn.cs.utexas.edu/downloads/papers/stanley.gecco02_1.pdf
+
+import argparse
+import numpy as np
+import sys
+import pandas as pd
+from sklearn import ensemble
+import SnakeGame
+
+def TrainModel(model, x, y, FutureState):
+	q= y+GetBestAftermath(model, FutureState)
+	for i in range(args.loops): 
+		model.fit(x, y+GetBestAftermath(model, FutureState) ) 
+		q= y+GetBestAftermath(model, FutureState)
+		q_hat= model.predict(x) 
+		print ("Iteration",i+1, 100*np.cov(q,q_hat)[1,0]/np.var(q) , "percent var retained by model.")
+		
+def GetBestAftermath(Q, FutureState):
+	Aftermaths=np.zeros((FutureState.shape[0],3))
+	# Allocate memory now so we dont need to copy arrays later.
+	# These arrays can get big
+	for i in range(3):
+		FutureState[:, -3:]=SnakeGame.VectoriseDecision(i)
+		Aftermaths[:,i] = Q.predict(FutureState).reshape(-1) 
+	return np.max( Aftermaths, axis=1) 
+	
+x,y,FutureState=[],[],[]
+MostImportantStates = [[],[],[]]
+hunger = 99999
+def PlayGames(brain, games, silent=False, save=False, hunger=99999, TurnMax=400):
+	global x,y,FutureState, MostImportantStates
+	apples=[]
+	turns=[]
+	gifs=[]
+	for i in range( games):
+		gif, StateLog, RewardLog = SnakeGame.PlayGame(brain, TurnMax=TurnMax, Hunger=hunger, save=save) 
+		turns.append(len(RewardLog))
+		apples.append(sum(RewardLog)+1)
+		gifs.append(gif)
+		x+=StateLog[:-1]
+		y+=RewardLog
+		FutureState+=StateLog[1:]
+	"""ImportantStates=np.array(y)!=0
+	MostImportantStates[0]+=x[ImportantStates]
+	MostImportantStates[1]+=y[ImportantStates]
+	MostImportantStates[2]+=FutureState[ImportantStates]"""
+	if not silent: print ("Observed", len(y),"states, on average lived", np.mean( turns), "turns, ate", np.mean( apples), "times.") 
+	return 	apples, turns, gifs
+
+def Positive_Integer(value):
+	if not (int(value)==float(value) and int(value)>0):
+		raise argparse.ArgumentTypeError("%s is an invalid value" % value)
+	return int(value)
+
+parser = argparse.ArgumentParser(description='Trains an AI to play the popular ATARI game snake from scratch.  Note that the AI is not given any instructions about the rules of the game prior to play.  If the AI attempts and illegal move (going backwards, walking out of bounds, ect.) this is treated as a death.  Use command line options to adjust the settings and observe changes in snake performance.  Outputs highlight reel of each snake generation to gif.')
+parser.add_argument('-m', '--Map Size',  metavar='M', dest='MapSize',default=8, help='Width of the playable map', type=Positive_Integer)
+parser.add_argument('-i', '--Iterations',  metavar='l', dest='loops',default=3, help='Number of times model is trained per genration', type=Positive_Integer) 
+parser.add_argument('-t', '--Hunger',  metavar='H',   dest='Hunger',default=50, help='Maximum number of turns snake can go without eating and not die', type=Positive_Integer) 
+parser.add_argument('-G', '--Games',  metavar='N',    dest='Games',default=210, help='Number of games played per genration by a skilled snake', type=Positive_Integer) 
+parser.add_argument('-rG', '--Random Games',  metavar='R', dest='rGames',default=100, help='Number of games played per genration by an unskilled snake', type=Positive_Integer) 
+parser.add_argument('-g', '--Generations',  metavar='G',   dest='Generations',default=7, help='Number of generations to train', type=Positive_Integer) 
+parser.add_argument('-H', '--Highlights',  metavar='H',    dest='Highlight',default=1, help='Number of games to put in the highlight Reel', type=Positive_Integer) 
+parser.add_argument('-z', '--Gifsize',  metavar='Z',       dest='Size',default=600, help='Approximate width of highlight Reel gif in pixels', type=Positive_Integer)  
+
+print ()
+args = parser.parse_args()
+parser.print_help()
+print ()
+
+SnakeGame.MapSize=args.MapSize
+Px_Size=         int(600/args.MapSize)
+if Px_Size<1:
+         raise argparse.ArgumentTypeError("Invalid Gifsize") 
+
+# Get the first round of data by moving randomly
+PlayGames(lambda state: np.random.randint(3), args.rGames)
+
+params = {'n_estimators':500, 'max_depth':5, 'min_samples_split': 2,
+          'learning_rate': 0.01, 'loss': 'ls'}
+Q = ensemble.GradientBoostingRegressor(**params)
+Q.fit(np.array(x), y )
+TrainModel(Q, np.array(x), y  , np.array(FutureState))
+
+def MakeDecision(state):
+	Decision=0
+	BestAftermath_hat=0
+	for i in range(3):
+		Aftermath_hat=Q.predict(np.array(state+SnakeGame.VectoriseDecision(i)).reshape(1,-1) ) 
+		if  Aftermath_hat>BestAftermath_hat:
+			BestAftermath_hat=Aftermath_hat
+			Decision=i
+	return Decision
+	
+for Generation in range(args.Generations):
+#	x,y,FutureState=MostImportantStates
+#	MostImportantStates = [[],[],[]]
+	print ()
+	print ( "     Generation", Generation+1) 
+	PlayGames(lambda state: np.random.randint(3), args.rGames, silent=True) 
+	apples, turns, gifs = PlayGames(MakeDecision, args.Games) 
+
+	if max(turns) > args.Games and hunger>args.Hunger:
+		hunger=args.Hunger
+		print ("SNAKE PERFORMING WELL.  ACTIVATING HUNGER" )
+	apples, turns, gifs = PlayGames(MakeDecision, args.Highlight*2,TurnMax=99999, save=True, silent=True, hunger=hunger) 
+	HighlightReel=[]
+	for i in tuple( np.where(np.array(apples) >= sorted(apples)[-args.Highlight]))[0] :
+		HighlightReel+=gifs[i]		
+	SnakeGame.imageio.mimsave("Generation"+ str(  Generation+1 )+".gif", HighlightReel)
+	del HighlightReel
+	del gifs
+	SnakeGame.gc.collect()
+	if Generation < args.Generations-1:
+		TrainModel(Q, np.array(x), y  , np.array(FutureState))
+
+
+	
+
